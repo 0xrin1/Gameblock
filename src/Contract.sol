ccd24751e7747679c46edd42014b6f71e1b82ec8
diiil <mstuessyosu@gmail.com>
2022-02-08 23:50:21 +0000

    initial commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-contract Contract {}
+contract Contract {
+    string[] public states;
+
+    function addState(string calldata state) public {
+        states.push(state);
+    }
+}
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,11 +2,24 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "../../src/Contract.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Contract c;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        c = new Contract();
+    }
+
+    function testAddState() public {
+        c.addState("some state");
+        assertEq(c.states(0), "some state");
+    }
+
+    function testAddStates() public {
+        c.addState("some state");
+        c.addState("another state");
+        assertEq(c.states(0), "some state");
+        assertEq(c.states(1), "another state");
     }
 }
